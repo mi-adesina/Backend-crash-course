17d9d3a3db6c42b1f0841eac5b8846a647f3b8ff
mi-adesina <uniquecyborg@gmail.com>
2025-04-26 10:18:29 +0100

    just Node

---
+++ app.js
@@ -0,0 +1,20 @@
+// without express
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text/plain" });
+        res.end("Home Page");
+    } else if (req.url === "/about") {
+        res.writeHead(200, { "content-type": "text/plain" });
+        res.end('About Page');
+    } else {
+        res.writeHead(404, { 'content-type': 'text/plain' });
+        res.end('Page Not Found')
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
